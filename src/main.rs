d1192900d53f51f30dc048f1089c966fc564fe67
twil3akine <twil3akine@gmail.com>
2026-01-02 22:38:43 +0900

    MCPのリクエスト構造体を作成

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use serde::Deserialize;
+
+/// MCPのリクエスト構造
+#[derive(Deserialize)]
+struct JsonRcpRequest {
+    method: String,
+    params: Option<serde_json::Value>,
+    id: Option<serde_json::Value>,
 }
+
+fn main() {}
